f82de8459b11cfb483abaaeca3e5e50ebc8f93d6
ahmaddevatajulm <ahmaddevatajulmuluk@gmail.com>
2024-09-26 19:03:40 +0700

    first commit

--- server.js
+++ server.js
@@ -1,49 +1,31 @@
 const express = require("express");
-const { Server } = require("json-rpc-2.0");
-const net = require("net");
-const path = require("path");
+const { JSONRPCServer } = require("json-rpc-2.0");
 
 const app = express();
-const port = 4000;
+const server = new JSONRPCServer();
 
-// Inisialisasi server RPC
-const rpcServer = new Server({
-  methods: {
-    add: ({ a, b }) => a + b,
-    subtract: ({ a, b }) => a - b,
-    multiply: ({ a, b }) => a * b,
-    divide: ({ a, b }) => {
-      if (b === 0) throw new Error("Division by zero");
-      return a / b;
-    },
-  },
+server.addMethod("add", ({ a, b }) => a + b);
+server.addMethod("subtract", ({ a, b }) => a - b);
+server.addMethod("multiply", ({ a, b }) => a * b);
+server.addMethod("divide", ({ a, b }) => {
+  if (b === 0) throw new Error("Division by zero");
+  return a / b;
 });
 
-// Menyajikan halaman HTML
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.static("public"));
 
-app.get('/rpc', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-// Membuat server TCP untuk menangani request RPC
-const tcpServer = net.createServer((socket) => {
-  socket.on("data", (data) => {
-    const request = data.toString();
-    rpcServer.receive(request).then((response) => {
-      if (response) {
-        socket.write(JSON.stringify(response));
-      }
-    });
+app.post("/rpc", (req, res) => {
+  server.receive(req.body).then((jsonRPCResponse) => {
+    if (jsonRPCResponse) {
+      res.json(jsonRPCResponse);
+    } else {
+      res.sendStatus(204);
+    }
   });
 });
 
-// Menjalankan server pada port 4000
-tcpServer.listen(4000, () => {
-  console.log("RPC Kalkulator server berjalan di port 4000");
-});
-
-// Menjalankan server web pada port 3000
-app.listen(3000, () => {
-  console.log("Server web berjalan di port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
